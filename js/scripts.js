223f795c832dbe748c4e62346f9908b7a950aabf
Morgan Devish <morgandevish@Morgans-MacBook-Pro.local>
2019-06-03 16:43:29 -0400

    fixed the javascript for the menu

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ console.log("Hi, welcome to my Portfolio Site!");
 function menuToggle() {
   var x = document.getElementById("myNavtoggle");
   if (x.className === "navtoggle") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "navtoggle";
   }
